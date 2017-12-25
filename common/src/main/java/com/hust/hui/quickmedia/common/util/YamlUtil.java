5ebc3ccab520c2653828f2818519fed6e85d380d
yihui <yihui@mogujie.com>
2017-12-25 17:31:25 +0800

    fix

--- common/src/main/java/com/hust/hui/quickmedia/common/util/YamlUtil.java
+++ common/src/main/java/com/hust/hui/quickmedia/common/util/YamlUtil.java
@@ -15,19 +15,20 @@ public class YamlUtil {
      * yaml文件读取
      *
      * @param fileName 文件名
-     * @param clz 格式化的对象实例
+     * @param clz      格式化的对象实例
      * @param <T>
      * @return
      * @throws IOException
      */
     public static <T> T read(String fileName, Class<T> clz) throws IOException {
-        try (InputStream inputStream = FileReadUtil.getStreamByFileName(fileName)) {
-            Yaml yaml = new Yaml();
-            return yaml.loadAs(inputStream, clz);
-        }
+        InputStream inputStream = FileReadUtil.getStreamByFileName(fileName);
+        Yaml yaml = new Yaml();
+        T t = yaml.loadAs(inputStream, clz);
+        inputStream.close();
+        return t;
     }
 
-    public  static Object read(String fileName) throws IOException {
+    public static Object read(String fileName) throws IOException {
         try (InputStream inputStream = FileReadUtil.getStreamByFileName(fileName)) {
             Yaml yaml = new Yaml();
             return yaml.load(inputStream);
