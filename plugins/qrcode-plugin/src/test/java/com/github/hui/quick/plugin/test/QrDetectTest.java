d1c73fe2c2d395511a28275752b6c5c5c1734293
yihui <bangzewu@126.com>
2020-04-03 19:01:16 +0800

    update springboot version

--- plugins/qrcode-plugin/src/test/java/com/github/hui/quick/plugin/test/QrDetectTest.java
+++ plugins/qrcode-plugin/src/test/java/com/github/hui/quick/plugin/test/QrDetectTest.java
@@ -3,6 +3,7 @@ package com.github.hui.quick.plugin.test;
 import com.github.hui.quick.plugin.base.Base64Util;
 import com.github.hui.quick.plugin.base.ImageLoadUtil;
 import com.github.hui.quick.plugin.qrcode.wrapper.QrCodeGenWrapper;
+import com.github.hui.quick.plugin.qrcode.wrapper.QrCodeOptions;
 import com.google.zxing.WriterException;
 import org.junit.Test;
 
@@ -23,12 +24,40 @@ public class QrDetectTest {
 
     @Test
     public void testGenQr() throws IOException, WriterException {
-        BufferedImage bf = QrCodeGenWrapper.of(msg)
-                .setDrawBgColor(Color.WHITE)
-                .setDrawPreColor(Color.BLACK)
-                .setDetectInColor(Color.RED)
-                .asBufferedImage();
-        System.out.println("over");
+//        BufferedImage bf = QrCodeGenWrapper.of(msg)
+//                .setDrawBgColor(Color.WHITE)
+//                .setDrawPreColor(Color.BLACK)
+//                .setDetectInColor(Color.RED)
+//                .asBufferedImage();
+//        System.out.println("over");
+
+        String logo = "http://img1.imgtn.bdimg.com/it/u=2018939532,1617516463&fm=26&gp=0.jpg";
+        String msg = "https://liuyueyi.github.io/hexblog/2018/03/23/mysql之锁与事务详解/";
+        int size = 500;
+        try {
+
+            boolean ans = QrCodeGenWrapper.of(msg)
+                    .setW(300)
+                    .setDrawPreColor(0xff0000ff)
+                    .setDrawBgColor(0xffffffff)
+                    .setDetectOutColor(0xff00FF00)
+                    .setDetectInColor(0xffff0000)
+                    .setPadding(1)
+                    .setLogo(logo)
+                    .setLogoRate(12)
+                    .setLogoStyle(QrCodeOptions.LogoStyle.ROUND)
+                    .setLogoBorder(true)
+                    .setLogoBgColor(Color.GREEN)
+                    .setBgOpacity(0.8f)
+                    .setBgStyle(QrCodeOptions.BgImgStyle.FILL)
+                    .setBgStartX(108)
+                    .setBgStartY(40)
+                    .asFile("/tmp/yyy.png");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
