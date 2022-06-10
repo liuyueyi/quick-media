36be9e81fdeffe638b7bacfc2c5577ec63fefc37
yihui <bangzewu@126.com>
2022-06-10 15:01:30 +0800

    add svg demo

--- plugins/qrcode-plugin/src/main/java/com/github/hui/quick/plugin/qrcode/helper/QrCodeRenderHelper.java
+++ plugins/qrcode-plugin/src/main/java/com/github/hui/quick/plugin/qrcode/helper/QrCodeRenderHelper.java
@@ -264,22 +264,12 @@ public class QrCodeRenderHelper {
                 // 选择一块区域进行填充
                 bgGraphic.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_ATOP, 1.0f));
                 bgGraphic.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-//                bgGraphic
-//                        .drawImage(qrImg.getScaledInstance(qrWidth, qrHeight, Image.SCALE_SMOOTH), bgOffsetX, bgOffsetY,
-//                                null);
-                // 实验功能，用于gif生成时缩放
-                int add = 2 * Math.abs(index - len / 2);
-                int newQrW = qrWidth + add;
-                int newQrH = qrHeight + add;
-
-                bgGraphic.drawImage(qrImg.getScaledInstance(newQrW, newQrH, Image.SCALE_SMOOTH), bgOffsetX - add / 2, bgOffsetY - add /2, null);
+                bgGraphic.drawImage(qrImg.getScaledInstance(qrWidth, qrHeight, Image.SCALE_SMOOTH), bgOffsetX, bgOffsetY, null);
             } else {
                 // 全覆盖模式, 设置透明度， 避免看不到背景
                 bgGraphic.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_ATOP, bgImgOptions.getOpacity()));
                 bgGraphic.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-                bgGraphic
-                        .drawImage(qrImg.getScaledInstance(qrWidth, qrHeight, Image.SCALE_SMOOTH), bgOffsetX, bgOffsetY,
-                                null);
+                bgGraphic.drawImage(qrImg.getScaledInstance(qrWidth, qrHeight, Image.SCALE_SMOOTH), bgOffsetX, bgOffsetY, null);
                 bgGraphic.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_ATOP, 1.0f));
             }
             bgGraphic.dispose();
--- plugins/qrcode-plugin/src/main/java/com/github/hui/quick/plugin/qrcode/util/NumUtil.java
+++ plugins/qrcode-plugin/src/main/java/com/github/hui/quick/plugin/qrcode/util/NumUtil.java
@@ -9,10 +9,26 @@ import java.math.RoundingMode;
  */
 public class NumUtil {
 
+    /**
+     * 除，并向下取整
+     *
+     * @param a
+     * @param b
+     * @param scale
+     * @return
+     */
     public static float divWithScaleFloor(float a, float b, int scale) {
         return BigDecimal.valueOf(a).divide(BigDecimal.valueOf(b), scale, RoundingMode.FLOOR).floatValue();
     }
 
+    /**
+     * 乘，并向上取整
+     *
+     * @param a
+     * @param b
+     * @param scale
+     * @return
+     */
     public static float multiplyWithScaleFloor(float a, float b, int scale) {
         return BigDecimal.valueOf(a).multiply(BigDecimal.valueOf(b)).setScale(scale, RoundingMode.FLOOR).floatValue();
     }
