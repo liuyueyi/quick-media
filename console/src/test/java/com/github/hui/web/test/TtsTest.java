e67a81f3e7be17bbf726c7af2be2ab683b38ebc8
YiHui <bangzewu@126.com>
2024-04-23 14:15:02 +0800

    feat: 调整测试voice

--- console/src/test/java/com/github/hui/web/test/TtsTest.java
+++ console/src/test/java/com/github/hui/web/test/TtsTest.java
@@ -18,17 +18,17 @@ public class TtsTest {
         ts.setBaseSavePath("d:\\"); // 设置保存路径
         SSML ssml = SSML.builder()
                 .outputFormat(OutputFormat.audio_24khz_48kbitrate_mono_mp3)
-                .synthesisText("葡萄 Grape")
+                .synthesisText("Popcorn")
                 .outputFileName("grape")
-                .voice(VoiceEnum.zh_CN_XiaoxiaoNeural)
+                .voice(VoiceEnum.zh_CN_XiaoruiNeural)
                 .build();
         ts.sendText(ssml);
 
         ts.sendText(SSML.builder()
                 .outputFormat(OutputFormat.audio_24khz_48kbitrate_mono_mp3)
-                .synthesisText("我喜欢吃葡萄，I love to eat grapes")
+                .synthesisText("我喜欢吃葡萄，I love to eat Popcorn")
                 .outputFileName("grape sentence")
-                .voice(VoiceEnum.zh_CN_XiaoxiaoNeural)
+                .voice(VoiceEnum.zh_CN_XiaoruiNeural)
                 .build()
         );
 //
