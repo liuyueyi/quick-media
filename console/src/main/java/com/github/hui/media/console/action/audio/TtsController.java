dc16824d59b18d72a6faff4d94b5ce1b3f173047
YiHui <bangzewu@126.com>
2024-04-28 11:29:02 +0800

    feat: 支持语音选择

--- console/src/main/java/com/github/hui/media/console/action/audio/TtsController.java
+++ console/src/main/java/com/github/hui/media/console/action/audio/TtsController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
+ * 文本转tts
+ *
  * @author YiHui
  * @date 2024/4/22
  */
@@ -19,16 +21,25 @@ public class TtsController {
     private volatile long lastVisit = 0L;
 
     @RequestMapping(path = "tts")
-    public ResponseWrapper<String> toTts(String content, String saveFile) {
+    public ResponseWrapper<String> toTts(String content, String saveFile, String voice) {
         System.out.println("开始转换: " + saveFile);
         SSML ssml = SSML.builder()
                 .outputFormat(OutputFormat.audio_24khz_48kbitrate_mono_mp3)
                 .synthesisText(content)
                 .outputFileName(saveFile)
-                .voice(VoiceEnum.zh_CN_XiaoxiaoNeural)
+                .voice(buildVoice(voice))
                 .build();
         getTts().sendText(ssml);
         return ResponseWrapper.successReturn(BASE + saveFile + ".mp3");
+    }
+
+    private VoiceEnum buildVoice(String voice) {
+        for (VoiceEnum v : VoiceEnum.values()) {
+            if (v.name().equalsIgnoreCase(voice)) {
+                return v;
+            }
+        }
+        return VoiceEnum.zh_CN_XiaoxiaoNeural;
     }
 
     private void init() {
