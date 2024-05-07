1109352e0799fc2222633edeff2210b7e93910ae
YiHui <bangzewu@126.com>
2024-05-07 18:15:31 +0800

    feat: 移除无用测试

--- plugins/tts-plugin/src/test/java/Main.java
+++ plugins/tts-plugin/src/test/java/Main.java
@@ -9,23 +9,6 @@ import org.junit.Test;
  */
 public class Main {
     public static void main(String[] args) {
-//        TTSService ts = new TTSService();
-//        ts.setBaseSavePath("d:\\"); // 设置保存路径
-//        SSML ssml = SSML.builder()
-//                .outputFormat(OutputFormatEnum.audio_24khz_48kbitrate_mono_mp3)
-//                .synthesisText("自定义文件名测试文本，java 文本转语音")
-//                .outputFileName("自定义文件名")
-//                .voice(VoiceEnum.zh_CN_XiaoxiaoNeural)
-//                .build();
-//        ts.sendText(ssml);
-//
-//        ts.sendText(SSML.builder()
-//                .synthesisText("文件名自动生成测试文本")
-//                .usePlayer(true) // 语音播放
-//                .build());
-//
-//        ts.close();
-
         TtsWrapper.sendTxt(TtsConfig.newConfig()
                 .outputFileName("测试")
                 .setSsml("我喜欢吃葡萄 I like to eat grape")
